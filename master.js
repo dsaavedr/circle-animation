33d2da6c204b64327f3c83243ca94b28130c7638
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-09-07 15:07:52 -0500

    Finished animation with scalable vertices. Pending adding user interaction for toggling elements

--- master.js
+++ master.js
@@ -1,11 +1,14 @@
 var r = 250,
-    miniR = 10,
+    miniR = 8,
     delta = 0.025,
     angle = Math.PI,
+    s = 1,
+    val = 1,
     graphOffset = 20,
     cSin = "#fd2f24",
     cCos = "#fed800",
-    c = "#ff6f01";
+    c = "#ff6f01",
+    n = 3;
 
 var canvas = document.getElementById("canvas"),
     ctx = canvas.getContext("2d");
@@ -29,13 +32,13 @@ function init() {
 
     ctx.strokeStyle = "white";
     ctx.lineWidth = 3;
-    ctx.translate(WIDTH / 4, HEIGHT / 2);
+    ctx.translate(WIDTH / 2, HEIGHT / 2);
 
     ani();
 }
 
 function ani() {
-    ctx.fillRect(-WIDTH / 4, -HEIGHT / 2, WIDTH, HEIGHT);
+    ctx.fillRect(-WIDTH / 2, -HEIGHT / 2, WIDTH, HEIGHT);
 
     ctx.beginPath();
     ctx.arc(0, 0, r, 0, 2 * Math.PI, true);
@@ -48,52 +51,39 @@ function ani() {
     ctx.save();
     ctx.lineWidth = 1;
     ctx.beginPath();
-    ctx.moveTo(0, -r);
-    ctx.lineTo(0, r);
-    ctx.moveTo(-r, 0);
-    ctx.lineTo(r, 0);
-    ctx.moveTo(0, v.y);
-    ctx.lineTo(v.x, 0);
-    ctx.stroke();
-    ctx.closePath();
-    ctx.restore();
-
-    ctx.save();
-    ctx.lineWidth = 1;
-    ctx.translate(WIDTH / 4, 0);
-    ctx.moveTo(0, 0);
-    ctx.lineTo(graphOffset + 2 * r, 0);
-    ctx.moveTo(graphOffset, -r);
-    ctx.lineTo(graphOffset, r);
-    ctx.stroke();
-    ctx.restore();
-
-    ctx.save();
-    ctx.lineWidth = 1;
-    ctx.translate(WIDTH / 4 + graphOffset, 0);
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-
-    for (var i = 0; i <= Math.PI * 2; i += delta) {
-        ctx.lineTo(scale(i, 0, Math.PI * 2, 0, r * 2), Math.sin(i) * r);
+    for (var i = 0; i < n; i++) {
+        var vec = Vector.fromAngle((Math.PI / n) * i);
+        vec.setMag(r);
+        ctx.moveTo(vec.x, vec.y);
+        ctx.lineTo(-vec.x, -vec.y);
     }
-    ctx.moveTo(0, 0);
-    for (var i = 0; i <= Math.PI * 2; i += delta) {
-        ctx.lineTo(scale(i, 0, Math.PI * 2, 0, r * 2), Math.cos(i) * r);
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+
+    ctx.moveTo(v.x, 0);
+    for (var i = 1; i <= n; i++) {
+        ctx.save();
+        ctx.lineWidth = 1;
+        var ang = (Math.PI / n) * i;
+        ctx.rotate(ang);
+        ctx.lineTo(Math.cos(angle - ang) * r, 0);
+        ctx.stroke();
+        ctx.restore();
     }
 
-    ctx.stroke();
-    ctx.closePath();
-    ctx.restore();
+    for (var i = 0; i < n; i++) {
+        var ang = (Math.PI / n) * i;
+        // var h = scale(ang % (Math.PI * 2), 0, Math.PI * 2, 0, 1);
+        // var color = HSVtoRGB(h, s, val);
+        ctx.save();
+        ctx.rotate(ang);
+        point(Math.cos(angle - ang) * r, 0, cCos, miniR);
+        // point(Math.cos(angle - ang) * r, 0, color, miniR);
+        ctx.restore();
+    }
 
-    point(v.x, 0, cCos, miniR);
-    point(0, v.y, cSin, miniR);
-
-    var x = WIDTH / 4 + graphOffset + scale(angle % (Math.PI * 2), 0, Math.PI * 2, 0, r * 2);
-    point(x, v.x, cCos, miniR);
-    point(x, v.y, cSin, miniR);
-
-    point(v.x, v.y, c, miniR * 1.2);
+    // point(v.x, v.y, c, miniR * 1.2);
 
     angle += delta;
 
